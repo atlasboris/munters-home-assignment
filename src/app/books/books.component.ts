14f3248a40f9519ad0db22f15fd7ce8aadc05ae5
atlasboris <atlasboris@gmail.com>
2023-10-22 09:17:39 +0300

    responsive books table

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.scss']
+  styleUrls: ['./books.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BooksComponent {
 
